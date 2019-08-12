a1ff8f802e8e6c9f94a2607c6c1afc6272acdc2c
Marcus Santos <marcusdcarli@gmail.com>
2019-08-12 09:01:28 -0300

    Aula 8

--- cdc-admin/src/Componentes/InputCustomizado.js
+++ cdc-admin/src/Componentes/InputCustomizado.js
@@ -12,7 +12,7 @@ import PubSub from 'pubsub-js';
         return(
             <div className="pure-control-group">
                 <label htmlFor={this.props.id}>{this.props.label}</label> 
-                <input id={this.props.id} type={this.props.type} name={this.props.name} value={this.props.value} onChange={this.props.onChange} />
+                <input {...TouchList.props} />
                 <span className="erro">{this.state.msgErro}</span>                  
             </div>
         );
